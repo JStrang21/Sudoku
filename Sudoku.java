ed83a4d6c8feb9d95dc308c90dddd66a9e39acb0
Justin Strang <Justinstrang100@gmail.com>
2023-02-12 12:48:10 -0500

    Read in a board from input and print result

--- Sudoku.java
+++ Sudoku.java
@@ -2,8 +2,43 @@ import java.util.*;
 
 public class Sudoku
 {
+    /*
+     * 
+     * 
+     * Justin P. Strang
+     * EECS 1510 Sudoku Project
+     * First Commit 02/12/2023
+     *      Will begin by getting one board to work
+     */
     public static void main(String[] args)
     {
-        
+        //Create a scanner to get user input
+        Scanner input = new Scanner(System.in);
+        //Prompt user for input
+        System.out.println("Enter three 9x9 matrices: ");
+        //Instantiate board variables which will have input read into
+        int[][] boardOne = new int[9][9];
+        //Use for loop to read in user input to board variable
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                boardOne[i][j] = input.nextInt();
+            }
+        }
+        //Test print user input
+        printBoard(boardOne);
+    }
+
+    public static void printBoard(int[][] board)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
