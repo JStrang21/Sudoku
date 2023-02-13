b2e970100a527b1939c101739006f06adac725ef
Justin Strang <Justinstrang100@gmail.com>
2023-02-13 00:54:12 -0500

    Seting up method structure/breaking down problem into smaller chunks

--- Sudoku.java
+++ Sudoku.java
@@ -2,13 +2,24 @@ import java.util.*;
 
 public class Sudoku
 {
-    /*
+    /*  Test Board
+        7 2 4 8 6 5 1 3 9
+        5 1 9 2 4 3 8 7 6
+        3 0 6 7 9 1 5 4 2
+        1 7 8 6 2 9 4 5 3
+        9 4 3 1 5 8 2 6 7
+        6 5 2 3 7 4 9 1 8
+        2 3 1 5 8 6 7 9 4
+        8 9 5 4 3 7 6 2 1
+        4 6 7 9 1 2 3 8 5
      * 
      * 
      * Justin P. Strang
      * EECS 1510 Sudoku Project
+     * Sudoku solver project where the program is read in a number of 9x9 matrices and outputs the numbers needed to solve the matrices
      * First Commit 02/12/2023
      *      Will begin by getting one board to work
+     *      Maybe use int[][][] to put all boards in a single variable
      */
     public static void main(String[] args)
     {
@@ -26,8 +37,31 @@ public class Sudoku
                 boardOne[i][j] = input.nextInt();
             }
         }
+        
         //Test print user input
-        printBoard(boardOne);
+        //System.out.println("//////////");
+        //printBoard(boardOne);
+        
+        //Send board to board solver method
+        boardSolver(boardOne);
+
+    }
+
+    public static void boardSolver(int[][] board)
+    {
+        //To solver needs to find squares with missing values, recognize the problem type, check rows and columns 
+        //Send board to another method to find the missing values and return what type of problem it is and where the missing values are located
+        //Store in int[][] variable where column of three represents the three possible missing values and the row represents the location of the value
+        int[][] locationOfMissingValues = new int[3][3];
+        locationOfMissingValues = findTypeOfMissingValue(board);
+        //If type 1 solve by checking column/row
+
+    }
+
+    public static int[][] findTypeOfMissingValue(int[][] board)
+    {
+        int[][] locationOfMissingValues = new int[3][3];
+        return locationOfMissingValues;
     }
 
     public static void printBoard(int[][] board)
