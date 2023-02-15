a6b3794010a82ada62aae3417d74b6b30af739e8
Justin Strang <Justinstrang100@gmail.com>
2023-02-15 16:42:05 -0500

    Added method to find location of missing values

--- Sudoku.java
+++ Sudoku.java
@@ -3,15 +3,15 @@ import java.util.*;
 public class Sudoku
 {
     /*  Test Board
-        7 2 4 8 6 5 1 3 9
-        5 1 9 2 4 3 8 7 6
-        3 0 6 7 9 1 5 4 2
-        1 7 8 6 2 9 4 5 3
-        9 4 3 1 5 8 2 6 7
-        6 5 2 3 7 4 9 1 8
-        2 3 1 5 8 6 7 9 4
-        8 9 5 4 3 7 6 2 1
-        4 6 7 9 1 2 3 8 5
+7 2 4 8 6 5 1 3 9
+5 1 9 2 4 3 8 7 6
+3 0 6 7 9 1 5 4 2
+1 7 8 6 2 9 4 5 3
+9 4 3 1 5 8 2 6 7
+6 5 2 3 7 4 9 1 8
+2 3 1 5 8 6 7 9 4
+8 9 5 4 3 7 6 2 1
+4 6 7 9 1 2 3 8 5
      * 
      * 
      * Justin P. Strang
@@ -54,16 +54,88 @@ public class Sudoku
         //Store in int[][] variable where column of three represents the three possible missing values and the row represents the location of the value
         int[][] locationOfMissingValues = new int[3][3];
         locationOfMissingValues = findTypeOfMissingValue(board);
-        //If type 1 solve by checking column/row
+       
+        //Print Check: location[x][1] = row && location[x][2] = column
+        //System.out.println(locationOfMissingValues[0][1]);
+        //System.out.println(locationOfMissingValues[0][2]);
+
+        //Number of missing values found determines which type of solver the board is sent to
+        if (locationOfMissingValues[2][0] == 1)
+        {
+            typeThreeSolver(board, locationOfMissingValues);
+        }
+        if (locationOfMissingValues[1][0] == 1 && locationOfMissingValues[2][0] == 0)
+        {
+            typeTwoSolver(board, locationOfMissingValues);
+        }
+        if (locationOfMissingValues[0][0] == 1 && locationOfMissingValues[1][0] == 0)
+        {
+            typeOneSolver(board, locationOfMissingValues);
+        }
 
     }
 
     public static int[][] findTypeOfMissingValue(int[][] board)
     {
+        //Create array to store missing value locations
         int[][] locationOfMissingValues = new int[3][3];
+        int numberMissing = 0;
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board[i].length; j++)
+            {
+                //If square has missing value
+                if (board[i][j] == 0)
+                {
+                    //Set first column = to 1 to indicate missing value
+                    locationOfMissingValues[numberMissing][0] = 1;
+                    //Second column is the row location
+                    locationOfMissingValues[numberMissing][1] = i;
+                    //Third column is the column location
+                    locationOfMissingValues[numberMissing][2] = j;
+                    numberMissing++;
+                }
+            }
+        }
         return locationOfMissingValues;
     }
 
+    public static void typeThreeSolver(int[][] board, int[][] locationOfMissingValues)
+    {
+
+    }
+
+    public static void typeTwoSolver(int[][] board, int[][] locationOfMissingValues)
+    {
+
+    }
+
+    public static void typeOneSolver(int[][] board, int[][] locationOfMissingValues)
+    {
+        //Arrays to hold values of row and column of missing value
+        int[] column = new int[9];
+        int[] row = new int[9];
+        
+        int columnLocation = locationOfMissingValues[0][2];
+        int rowLocation = locationOfMissingValues[0][1];
+        //Loop through and add present values to row and column arrays
+        for (int i = 0; i < 9; i++)
+        {
+            row[i] = board[rowLocation][i];
+            column[i] = board[i][columnLocation];
+        }
+        
+        //Sort column and row array
+
+        //Compare sorted arrays to find value of missing value
+    }
+    
+    
+    
+    
+    
+    
+    
     public static void printBoard(int[][] board)
     {
         for (int i = 0; i < 9; i++)
